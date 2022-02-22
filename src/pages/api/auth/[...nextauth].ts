f70270964ed2d6bdee1f495cf470362ee9ce441d
sanderdsz <sanderdsz@gmail.com>
2022-02-21 21:28:04 -0300

    using faunadb to save user email

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,49 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            scope: 'read:user'
-        }),
-        // ...add more providers here
-    ],
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user'
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (e) {
+        console.log(e)
+        return false
+      }
+    },
+  }
 })
