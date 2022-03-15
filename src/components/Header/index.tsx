13ce028e15374c59696d42c1d4c2e1837ea19169
sanderdsz <sanderdsz@gmail.com>
2022-03-14 21:27:13 -0300

    routes and active routes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
+import Link from 'next/link'
+import {useRouter} from "next/router";
 import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
 
 import styles from './styles.module.scss'
 
 export function Header() {
+  const { asPath } = useRouter()
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -49,7 +49,6 @@ export const getStaticProps: GetStaticProps = async () => {
   )
 
   const posts = response.results.map(post => {
-    console.log(post)
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
