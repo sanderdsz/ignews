f50000ded0b2d96ba2a68d953de4f873c4dc16e0
sanderdsz <sanderdsz@gmail.com>
2022-03-16 21:31:18 -0300

    post page and styles

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,57 @@
+import {GetServerSideProps} from "next";
+import {getSession} from "next-auth/client";
+import { client } from "../../services/prismic";
+import {RichText} from "prismic-dom";
+import Head from 'next/head'
+
+import styles from './styles.module.scss'
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  return(
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.content }} />
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params}) => {
+  const session = await getSession({req})
+  const { slug } = params;
+  const response = await client.getByUID('post', String(slug), {})
+  console.log(response)
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import {GetStaticProps} from "next";
 import { client } from "../../services/prismic";
 import * as prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -26,11 +27,13 @@ export default function Posts({ posts } : PostsProps) {
         <div className={styles.posts}>
           {posts.map(post => (
             // eslint-disable-next-line react/jsx-key
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
